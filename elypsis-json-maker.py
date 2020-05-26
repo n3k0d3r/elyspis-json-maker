4ea1b00dca6387a45298bd34e1cdef8aba947e62
Sebastian <darklink.perry@gmail.com>
2020-05-25 22:36:47 -0400

    live config, read in colors

--- elypsis-json-maker.py
+++ elypsis-json-maker.py
@@ -2,6 +2,7 @@
 
 import sys
 import os.path
+import shutil
 import re
 
 # Check for only 1 argument
@@ -15,34 +16,35 @@ if(not os.path.exists(filename)):
     print("File '%s' does not exist" % filename)
     exit()
 
-# Color variants
-colors = {
-    "white",
-    "orange",
-    "magenta",
-    "light_blue",
-    "yellow",
-    "lime",
-    "pink",
-    "gray",
-    "light_gray",
-    "cyan",
-    "blue",
-    "purple",
-    "green",
-    "brown",
-    "red",
-    "black"
-}
+# Output directory
+output_dir = "output"
+if(os.path.exists(output_dir)):
+    shutil.rmtree(output_dir)
+os.makedirs(output_dir)
 
 # Open and read in file given
+colors = {}
+stonecutting = {}
+shapeless = {}
+shaped = {}
+setting = ""
+d_char = ":"
+sub_d_char = ";"
 print("Opening file '%s'" % filename)
 f = open(filename, "r")
-config = f.read().split(";")
+for line in f:
+    if(re.search(r"\[.*\]", line)):
+        setting = re.search(r"(?<=\[).*(?=\])", line).group()
+    else:
+        if(setting == "colors"):
+            color = line[:line.find(d_char)].strip()
+            dye = line[line.find(d_char) + 1:].strip()
+            colors[color] = dye
 f.close()
+print(colors)
 
 # Strip whitespace, pull colored variants and add to list in each color
-config = [each.strip() for each in config]
+'''config = [each.strip() for each in config]
 variants = [each for each in config if re.search("@color", each)]
 config = [each for each in config if each not in variants]
 for each in variants:
@@ -51,4 +53,4 @@ for each in variants:
 
 
 print(config)
-print(variants)
+print(variants)'''
